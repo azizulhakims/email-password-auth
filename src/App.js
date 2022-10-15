acc9b232260bd802fa168a607ffc6495d09c36e3
azizulhakims <it.azizulhakim@gmsil.com>
2022-10-15 10:20:37 +0600

    add project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { getAuth } from 'firebase/auth'
+import './App.css';
+import app from './firebase/firebase.init';
+
+const auth = getAuth(app)
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBwMgY0Tjoj1bQoi4I2tHW4vq0f9EeIOOI",
+    authDomain: "email-password-auth-249a4.firebaseapp.com",
+    projectId: "email-password-auth-249a4",
+    storageBucket: "email-password-auth-249a4.appspot.com",
+    messagingSenderId: "96365604603",
+    appId: "1:96365604603:web:1dcdba848f3200265aca5e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
