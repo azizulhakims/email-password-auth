994e1ad4a214fe5b7aceda7bde3ea245cfb16461
azizulhakims <it.azizulhakim@gmsil.com>
2022-10-15 10:54:58 +0600

    add onsubmit & onblur

--- src/App.js
+++ src/App.js
@@ -3,11 +3,31 @@ import './App.css';
 import app from './firebase/firebase.init';
 
 const auth = getAuth(app)
+const handleRegister = (event) => {
+  const email = event.target.email.value;
+  const password = event.target.password.value;
+  event.preventDefault();
+  console.log(email, password)
+}
+
+const handleEmailBlur = event => {
+  console.log(event.target.value);
+}
+
+const handlePasswordChange = event => {
+  console.log(event.target.value);
+}
 
 function App() {
   return (
     <div className="App">
-
+      <form onSubmit={handleRegister}>
+        <input onBlur={handleEmailBlur} type="email" name="email" placeholder='Your Email' id="" />
+        <br />
+        <input onChange={handlePasswordChange} type="password" name="password" placeholder='Your Password' id="" />
+        <br />
+        <button type='submit'>Resister</button>
+      </form>
     </div>
   );
 }
--- src/_steps.js
+++ src/_steps.js
@@ -1,6 +1,12 @@
 /*
+Auth terminology 
 "Register" or sign up [first time create a user]
 log in or sign in [already you have a user account created]
 
+
+get form value: 
+set form noSubmitHandler
+ser event parameter
+event.target.email.value
 
 */
